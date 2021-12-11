d46edf6dc1b9bbbe87ae988305a5e40071230de2
SA-Bijoy <bijoykhanpp@gmail.com>
2021-12-11 21:46:34 +0600

    added two file

--- hello.c
+++ hello.c
@@ -1,8 +1,34 @@
+// password all word by word check program //
+
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
-	printf("hello world!\n");
+	char password[30];
+	printf("Enter a new password: \n");
+	fflush(stdout);
+
+	gets(password);
+
+	char again_password[30];
+	printf("Enter your password again: \n");
+	fflush(stdout);
+
+	gets(again_password);
+
+	int i,j;
+
+	int pass_length = strlen(password);
+
+	for(i = 0,j = 0; i < pass_length; i++,j++){
+		if(password[i] == again_password[j]){
+			printf("%d word matched\n", i+1);
+		}else{
+			printf("%d word don't matched\n", i+1);
+		}
+	}
+	
 
 	return 0;
 }
